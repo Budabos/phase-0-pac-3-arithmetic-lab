891d1611a7b025e36e3fe959cee03f23f109a860
caroline opiyo <opiyocaroline20@gmail.com>
2023-09-17 21:48:19 +0300

    arithmetic lab

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(value1,value2){
+    return value1 + value2;
+}
+add(4 ,5);
+function subtract(value1,value2){
+    return value1-value2;
+}
+subtract(7,2);
+
+function divide(value1,value2){
+    return value1/value2;
+}
+divide(7,2);
+
+function multiply(value1,value2){
+    return value1*value2;
+}
+multiply(7,2);
+
+function increment(n){
+return n+1;
+}
+increment(2);
+
+function decrement(n){
+    return n-1;
+    }
+    decrement(5);
+
+    function makeInt(str) {
+        return parseInt(str, 10);
+      }
+      
+       makeInt("32"); 
+
+       function preserveDecimal(str) {
+        return parseFloat(str);
+      }
+      
+      const result = preserveDecimal("3.14");
